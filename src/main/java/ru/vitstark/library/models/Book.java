fd3f0182827304b4e193161a03bbe65770fff5f8
Vitstark <vitstark@mail.ru>
2022-08-19 23:46:58 +0300

    models updated

--- src/main/java/ru/vitstark/library/models/Book.java
+++ src/main/java/ru/vitstark/library/models/Book.java
@@ -1,30 +1,45 @@
 package ru.vitstark.library.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import javax.persistence.*;
 import javax.validation.constraints.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "book")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
     private Long id;
 
     @NotEmpty(message = "Название не должно быть пустым")
     @Size(min = 2, max = 50, message = "Название должно иметь от 2 до 50 символов")
+    @Column(name = "name")
     private String name;
 
     @NotEmpty(message = "Имя автора не должно быть пустым")
     @Size(min = 2, max = 100, message = "Имя автора должно иметь от 2 до 100 символов")
+    @Column(name = "author")
     private String author;
 
     @NotNull(message = "Дата написания не должна быть пустой")
     @Max(value = 2022, message = "Дата написания должна быть до 2022")
+    @Column(name = "date")
     private Integer date;
 
-    private Long personId;
+    @ToString.Exclude
+    @ManyToOne
+    @JoinColumn(name = "person_id", referencedColumnName = "id")
+    private Person owner;
+
+    public Book(String name, String author, Integer date) {
+        this.name = name;
+        this.author = author;
+        this.date = date;
+    }
 }
--- src/main/java/ru/vitstark/library/models/Person.java
+++ src/main/java/ru/vitstark/library/models/Person.java
@@ -1,27 +1,40 @@
 package ru.vitstark.library.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import javax.persistence.*;
 import javax.validation.constraints.*;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "person")
 public class Person {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
     private Long id;
 
     @NotEmpty(message = "Имя не должно быть пустым")
     @Size(min = 2, max = 100, message = "Имя должно иметь от 2 до 100 символов")
+    @Column(name = "name")
     private String name;
 
     @NotNull(message = "Год рождения не должен быть пустой")
     @Max(value = 2022, message = "Год рождения должен быть в промежутке от 1900 до 2022")
     @Min(value = 1900, message = "Год рождения должен быть в промежутке от 1900 до 2022")
+    @Column(name = "year_of_birth")
     private Integer yearOfBirth;
 
+    @ToString.Exclude
+    @OneToMany(mappedBy = "owner")
+    private List<Book> books;
+
+    public Person(String name, Integer yearOfBirth) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+    }
 }
